1900fbeea519fb1ce9e08bd1d6d21335819047aa
Jason Lantz <jasontlantz@gmail.com>
2014-01-09 16:13:43 -0600

    Add migration for username/password on repository

---
+++ mrbelvedere/migrations/0004_auto__add_field_repository_username__add_field_repository_password.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Repository.username'
+        db.add_column(u'mrbelvedere_repository', 'username',
+                      self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+        # Adding field 'Repository.password'
+        db.add_column(u'mrbelvedere_repository', 'password',
+                      self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Repository.username'
+        db.delete_column(u'mrbelvedere_repository', 'username')
+
+        # Deleting field 'Repository.password'
+        db.delete_column(u'mrbelvedere_repository', 'password')
+
+
+    models = {
+        u'mrbelvedere.branch': {
+            'Meta': {'object_name': 'Branch'},
+            'github_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'jenkins_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'repository': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.Repository']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        },
+        u'mrbelvedere.branchjobtrigger': {
+            'Meta': {'object_name': 'BranchJobTrigger'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'branch': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.Branch']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.Job']"}),
+            'last_trigger_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'mrbelvedere.githubuser': {
+            'Meta': {'object_name': 'GithubUser'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        },
+        u'mrbelvedere.jenkinssite': {
+            'Meta': {'object_name': 'JenkinsSite'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '255'}),
+            'user': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'mrbelvedere.job': {
+            'Meta': {'object_name': 'Job'},
+            'auth_token': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.JenkinsSite']"}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        },
+        u'mrbelvedere.push': {
+            'Meta': {'object_name': 'Push'},
+            'branch': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.Branch']"}),
+            'commit_url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'github_user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.GithubUser']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.TextField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        },
+        u'mrbelvedere.repository': {
+            'Meta': {'object_name': 'Repository'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'owner': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
+        },
+        u'mrbelvedere.repositorynewbranchjob': {
+            'Meta': {'object_name': 'RepositoryNewBranchJob'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.Job']"}),
+            'prefix': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'repository': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['mrbelvedere.Repository']"})
+        }
+    }
+
+    complete_apps = ['mrbelvedere']
