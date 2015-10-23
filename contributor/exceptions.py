322be17b9ee9867f3470f78b1c074f5a52fa963e
Jason Lantz <jlantz@salesforce.com>
2015-10-22 19:05:50 -0500

    Fix custom validation

--- contributor/exceptions.py
+++ contributor/exceptions.py
@@ -4,7 +4,7 @@ class DefaultBranchSyncFailed(Exception):
     def __init__(self, message, response):
 
         # Call the base class constructor with the parameters it needs
-        super(ValidationError, self).__init__(message)
+        super(Exception, self).__init__(message)
 
         # Now for your custom code...
         self.response = response
