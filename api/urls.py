17a55f3de303034b0a8ca071a8e3fe27f115ef07
Jason Lantz <jlantz@salesforce.com>
2018-08-14 10:22:13 -0500

    Remove api-auth url endpoint

--- api/urls.py
+++ api/urls.py
@@ -14,5 +14,4 @@ router.register(r'version-content', views.PackageVersionContentViewSet)
 # Additionally, we include the login URLs for the browseable API.
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
